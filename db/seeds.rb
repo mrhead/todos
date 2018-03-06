a3cabbf0d9e61e3f494d40c72d07c31720287e0e
Patrik Bóna <patrik.bona@mrhead.sk>
2018-03-06 09:52:38 +0100

    Update DB seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,5 @@
-Todo.create!(name: "New to-do")
-Todo.create!(name: "Completed to-do", completed: true)
+Todo.create!(name: "I'm first!")
+Todo.create!(name: "Me second :(")
+Todo.create!(name: "Nah, third")
+Todo.create!(name: "I'm complete!", completed: true)
+Todo.create!(name: "Me too, so what?", completed: true)

573f86bd063a7d5c4d7af09283e4e2ff80aa05ae
Patrik Bóna <patrik.bona@mrhead.sk>
2021-11-20 22:31:04 +0100

    Well, this is embarrassing... 😅

--- app/javascript/controllers/checkbox_controller.js
+++ app/javascript/controllers/checkbox_controller.js
@@ -1,10 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  connect() {
-    console.log("checkobox controller")
-  }
-
   submit() {
     this.element.closest("form").requestSubmit();
   }
